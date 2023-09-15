23ebda0c88d868a1d3c33bec9eea89389e6c8f7f
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-15 16:03:58 +0300

    Initial Commit

---
+++ TagTextField/TagTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagTextFieldApp.swift
+//  TagTextField
+//
+//  Created by Yunus Emre Taşdemir on 15.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TagTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
